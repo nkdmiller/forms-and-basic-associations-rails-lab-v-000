4333274fcc8259822b7c494e31a8f3215fe95a92
Nate Miller <natekmiller96@gmail.com>
2018-09-10 20:34:03 +0000

    automatically backed up by learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -2,4 +2,11 @@ class Song < ActiveRecord::Base
   belongs_to :artist
   belongs_to :genre
   has_many :songs
+  def genre_name=(name)
+     self.genre = Genre.find_or_create_by(name: name)
+  end
+ 
+   def genre_name
+      self.genre ? self.genre.name : nil
+   end
 end
