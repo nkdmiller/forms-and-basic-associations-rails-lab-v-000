8897e74834af9b2c3d88884f02b2284a91f84a98
Nate Miller <natekmiller96@gmail.com>
2018-09-10 20:39:33 +0000

    automatically backed up by learn

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,10 +1,4 @@
 class Artist < ActiveRecord::Base
   has_many :songs
-  def artist_name=(name)
-     self.artist = Artist.find_or_create_by(name: name)
-  end
- 
-   def artist_name
-      self.artist ? self.artist.name : nil
-   end
+
 end
